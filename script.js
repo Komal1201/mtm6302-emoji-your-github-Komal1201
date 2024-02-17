124efe941e739af9ce9e8cec927996de33eb8a56
Komal1201 <122998359+Komal1201@users.noreply.github.com>
2024-02-16 23:43:52 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+const emojiGallery = document.getElementById('emoji-gallery');
+
+const emojis = [
+    "&#x1F600;", "&#x1F601;", "&#x1F602;", "&#x1F603;",
+    "&#x1F604;", "&#x1F605;", "&#x1F606;", "&#x1F607;",
+    "&#x1F608;", "&#x1F609;", "&#x1F60A;", "&#x1F60B;"
+];
+
+emojis.forEach(emoji => {
+    const emojiItem = document.createElement('div');
+    emojiItem.classList.add('emoji-item');
+    emojiItem.innerHTML = `${emoji} ${emoji.codePointAt(0).toString(16).toUpperCase()}`;
+    emojiGallery.appendChild(emojiItem);
+});
