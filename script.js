c46fb71d270bb5967276c71045eb5cd495a94f7b
Komal1201 <122998359+Komal1201@users.noreply.github.com>
2024-02-17 23:14:41 -0500

    Update script.js

--- script.js
+++ script.js
@@ -9,6 +9,5 @@ const emojis = [
 emojis.forEach(emoji => {
     const emojiItem = document.createElement('div');
     emojiItem.classList.add('emoji-item');
-    emojiItem.innerHTML = '${emoji}';
     emojiGallery.appendChild(emojiItem);
 });
