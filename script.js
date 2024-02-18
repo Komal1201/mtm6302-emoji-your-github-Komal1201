e4904b2f3b806a54c33e0bc51dea48296ac44578
Komal1201 <122998359+Komal1201@users.noreply.github.com>
2024-02-17 20:09:24 -0800

    Update script.js
    Back

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const emojiGallery = document.getElementById('emoji-gallery');
 
 const emojis = [
-    "&#x1F601;", "&#x1F602;", "&#x1F603;",
+    "&#x1F600;", "&#x1F601;", "&#x1F602;", "&#x1F603;",
     "&#x1F604;", "&#x1F605;", "&#x1F606;", "&#x1F607;",
     "&#x1F608;", "&#x1F609;", "&#x1F60A;", "&#x1F60B;"
 ];
